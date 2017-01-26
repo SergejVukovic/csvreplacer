b5f701108589080fd99c62108a611df50caa8c56
Praksa10 <Praksa10>
2017-01-26 14:53:05 +0100

    trying to make it spin

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $sheet = $objPHPExcel->getActiveSheet();
 $counter =0;
 $collection = array();
 
+
 thruTable('A');
 
 echo $counter;
@@ -15,15 +16,17 @@ echo $counter;
 function thruTable($row){
     global $sheet;
     $column = 1;
-    $cell = 'a';
-    while ($cell !='') {
+    $limit = 500;
+    while (($row.$column) < $row.$limit) {
         $cell = $sheet->getCell($row . $column)->getValue();
         $word = explode(" ",$cell);
         addUniqeWord($word);
-        if ($cell == '') {
-            $cell = $sheet->getCell($row . ($column + 1))->getValue();
+        if ($cell == '' && ($sheet->getCell($row.($column+1))->getValue())== '') {
+           return true;
         }
         $column++;
+        if($column == $limit)
+            return false; //ovde si stao treba nastaviti , skontaj kako da se poziva na svakih 500 .
     }
 }
 
@@ -40,10 +43,12 @@ function addUniqeWord($neWord)
 
 function checker ($word)
 {
-    if(count_chars($word)<3)
-        return false;
     $word = clean($word);
     $word = strtolower($word);
+    if(strlen($word)<=3)
+    {
+        return false;
+    }
     global $collection,$counter;
     for ($i=0;$i<count($collection);$i++) {
         if ($collection[$i] == $word){
