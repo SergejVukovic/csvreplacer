d209b817004a6ea46d8d83b899295fad48b9a5bb
Praksa10 <Praksa10>
2017-01-30 14:56:37 +0100

    reading files , left sorting and counitn to do!

--- index.php
+++ index.php
@@ -1,82 +1,69 @@
 <?php
 ini_set('max_execution_time',3000);
 
-//require 'vendor/phpoffice/phpexcel/Classes/PHPExcel/IOFactory.php';
-//$dataFfile = 'ogasi.xls';
-//$objPHPExcel = PHPExcel_IOFactory::load($dataFfile);
-//$sheet = $objPHPExcel->getActiveSheet();
-//
 
-$file = fopen("ogasi3.csv","r");
+$col = 0;
+$row = array();
+$counter = 0;
+$collection = array();
+$file = fopen("ogasi.csv","r");
 
 while(! feof($file))
 {
-    print_r(fgetcsv($file));
+    $row[$counter] = fgetcsv($file)[$col];
+    $words[$counter] = explode(" ",$row[$counter]);
+    cheker($words[$counter]);
+    array_push($collection,$words[$counter]);
+    $counter++;
+}
+fclose($file);
+
+
+
+function cheker($words)
+{
+    global $collection;
+    for ($i=0;$i<sizeof($words);$i++)
+    {
+        if(!(is_numeric($words[$i])) && sizeof($words[$i]>2))
+        {
+            if(sizeof($collection)<1)
+            {
+                array_push($collection)
+            }
+            for ($j=0;$j<sizeof($collection);$j++)
+            {
+
+            }
+        }
+        echo "<br>";
+    }
+
+//    global $collection;
+//    $words = explode(" ",$words);
+//    if(sizeof($collection)<0){
+//        array_push($collection,$words[0]);
+//    }
+//    for ($i=1;$i<sizeof($words);$i++)
+//    {
+//        for ($j=0;$j<sizeof($collection);$j++)
+//            {
+//                if(sizeof($words[$i])>3)
+//                {
+//                    if($collection[$j] == $words[$i])
+//                    {
+//                        return false;
+//                    }
+//                    array_push($collection,$words[$i]);
+//                }
+//            }
+//    }
 }
 
-fclose($file);
-//
-//$uniqueCounter =0;
-//$collection = array();
-//if(isset($_POST['column']) && isset($_POST['limit']))
-//{
-//    thruTable($_POST['column'],$_POST['limit']);
-//
-//}else
-//{
-//    $error = "please enter values in the field";
-//}
-//
-//include 'form.html.php';
-//function thruTable($row,$limit){
-//    global $sheet;
-//    $column = 1;
-//    while ($column < $limit) {
-//        $cell = $sheet->getCell($row . $column)->getValue();
-//        $word = explode(" ",$cell);
-//        addUniqeWord($word);
-//        if ($cell == '' && ($sheet->getCell($row.($column+1))->getValue())== '') {
-//           return true;
-//        }
-//        if($column == $limit)
-//            return false;
-//
-//        $column++;
-//    }
-//}
-//
-//function addUniqeWord($neWord)
-//{
-//    global $collection;
-//    for($i = 0;$i<count($neWord);$i++) {
-//        $helper = checker($neWord[$i]);
-//       if($helper)
-//           array_push($collection,$neWord[$i]);
-//
-//    }
-//}
-//
-//function checker ($word)
-//{
-//    $word = clean($word);
-//    $word = strtolower($word);
-//    if(strlen($word)<=3)
-//    {
-//        return false;
-//    }
-//    global $collection,$uniqueCounter;
-//    for ($i=0;$i<count($collection);$i++) {
-//        if ($collection[$i] == $word){
-//            return false;
-//        }
-//    }
-//    $uniqueCounter++;
-//    return true;
-//}
-//function clean($string) {
-//    $string = str_replace(' ', '-', $string);
-//    $string = str_replace('.', '', $string);
-//    $string=rtrim($string,',');
-//    $string = str_replace('/[0-9]+/','',$string);
-//    return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
-//}
+function clean($string) {
+    $string = str_replace(' ', '-', $string);
+    $string = str_replace('.', '', $string);
+    $string=rtrim($string,',');
+    $string = str_replace('/[0-9]+/','',$string);
+    return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+}
