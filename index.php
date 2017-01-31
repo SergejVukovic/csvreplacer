02e6980bc45d425271d8f0ec8da175858baeda1e
Praksa10 <Praksa10>
2017-01-31 08:39:29 +0100

    deleted composer and phpexcel class , also added associative array to filter

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $col = 0;
 $row = array();
 $counter = 0;
 $collection = array();
+$compare = array();
 $file = fopen("ogasi.csv","r");
 
 while(! feof($file))
@@ -13,51 +14,36 @@ while(! feof($file))
     $row[$counter] = fgetcsv($file)[$col];
     $words[$counter] = explode(" ",$row[$counter]);
     cheker($words[$counter]);
-    array_push($collection,$words[$counter]);
     $counter++;
 }
 fclose($file);
-
-
-
+var_dump($collection[0]);
 function cheker($words)
 {
-    global $collection;
-    for ($i=0;$i<sizeof($words);$i++)
+   global $collection;
+    for($i=0;$i<sizeof($words);$i++)
     {
-        if(!(is_numeric($words[$i])) && sizeof($words[$i]>2))
+        if((!is_numeric($words[$i])) && (strlen($words[$i])>3))
         {
-            if(sizeof($collection)<1)
-            {
-                array_push($collection)
-            }
-            for ($j=0;$j<sizeof($collection);$j++)
-            {
+            $collection[$i] = array('key'=>$i,'value'=>$words[$i]);
+        }
+    }
+}
 
+function addWords($collection)
+{
+    $repeater =0;
+    global $compare;
+        for($i=0;$i<sizeof($collection);$i++)
+        {
+            for ($j=$i+1;$j<sizeof($collection);$j++)
+            {
+                if($collection[$i] == $collection[$j])
+                {
+                    array_push($compare,[$collection[$i]=>$repeater++]);
+                }
             }
         }
-        echo "<br>";
-    }
-
-//    global $collection;
-//    $words = explode(" ",$words);
-//    if(sizeof($collection)<0){
-//        array_push($collection,$words[0]);
-//    }
-//    for ($i=1;$i<sizeof($words);$i++)
-//    {
-//        for ($j=0;$j<sizeof($collection);$j++)
-//            {
-//                if(sizeof($words[$i])>3)
-//                {
-//                    if($collection[$j] == $words[$i])
-//                    {
-//                        return false;
-//                    }
-//                    array_push($collection,$words[$i]);
-//                }
-//            }
-//    }
 }
 
 function clean($string) {
