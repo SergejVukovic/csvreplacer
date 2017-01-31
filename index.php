f7360c6bc18223d7ff82ed92f80a1d09d4908289
Praksa10 <Praksa10>
2017-01-31 11:08:46 +0100

    holy shit it works... Counts words in column and displays

--- index.php
+++ index.php
@@ -2,54 +2,40 @@
 ini_set('max_execution_time',3000);
 
 
-$col = 0;
+$col = 1;
 $row = array();
 $counter = 0;
+$num=0;
 $collection = array();
-$compare = array();
+$keywords = array();
 $file = fopen("ogasi.csv","r");
 
 while(! feof($file))
 {
     $row[$counter] = fgetcsv($file)[$col];
     $words[$counter] = explode(" ",$row[$counter]);
-    cheker($words[$counter]);
+    getWords($words[$counter]);
     $counter++;
 }
 fclose($file);
-var_dump($collection[0]);
-function cheker($words)
+$result = array_count_values($collection);
+arsort($result);
+
+foreach($result as $word => $count)
 {
-   global $collection;
+    echo $word." was found ".$count." time(s)<br/>";
+}
+function getWords($words)
+{
+    global $collection;
+    global $num;
     for($i=0;$i<sizeof($words);$i++)
     {
         if((!is_numeric($words[$i])) && (strlen($words[$i])>3))
         {
-            $collection[$i] = array('key'=>$i,'value'=>$words[$i]);
+            $collection[$num] = $words[$i];
+            $num++;
         }
     }
 }
 
-function addWords($collection)
-{
-    $repeater =0;
-    global $compare;
-        for($i=0;$i<sizeof($collection);$i++)
-        {
-            for ($j=$i+1;$j<sizeof($collection);$j++)
-            {
-                if($collection[$i] == $collection[$j])
-                {
-                    array_push($compare,[$collection[$i]=>$repeater++]);
-                }
-            }
-        }
-}
-
-function clean($string) {
-    $string = str_replace(' ', '-', $string);
-    $string = str_replace('.', '', $string);
-    $string=rtrim($string,',');
-    $string = str_replace('/[0-9]+/','',$string);
-    return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
-}
