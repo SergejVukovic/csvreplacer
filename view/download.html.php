acbe677b24826e9b6c7962508277fc57a2c03bed
Praksa10 <Praksa10>
2017-02-06 13:46:57 +0100

    added more bootstrap

--- view/download.html.php
+++ view/download.html.php
@@ -1,12 +1,15 @@
 <html>
 <head>
-    <title>Search</title>
+    <title>Download</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
+<div class="col-md-4">
     <form action="../controllers/download.php" method="post">
         <label>Press button to download your new file!</label>
         <input type="hidden" name="filename" value="<?php echo $newFileName ?>">
         <button type="submit" class="btn btn-success">Download</button>
     </form>
+</div>
 </body>
 </html>
